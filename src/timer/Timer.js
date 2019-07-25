40ffb46e2f7f0a0eec6599264d4df71b1855a5ed
Henry Chao <kaidouya0527@gmail.com>
2019-07-25 14:36:58 +0800

    fix5

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -18,9 +18,9 @@ class Timer extends Component {
     render() {
         return (
             <div className={style['timer-container']}>
-                <Counter workTime={0} seconds={10} restTime={5} isMove={this.state.open} />
+                <Counter workTime={0} seconds={5} restTime={5} changeState={this.toggle} isMove={this.state.open} />
                 <div className={style['circle-body']}>
-                    <div className={style['circle-inner']} onClick={this.toogle}>
+                    <div className={`${style['circle-inner']} ${this.state.open ? style['circle-inner-active'] : ''}`} onClick={this.toogle}>
                         <span className={style['play-btn']}>
                             <Icon className={style['arrow-right']}>arrow_right</Icon>
                             <span className={style['dot']}></span>
